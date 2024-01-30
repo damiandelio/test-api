2572585067c322c3bbc1b7edfd2ef785bb1bc3de
damiandelio <damiandelio@gmail.com>
2024-01-30 20:24:36 -0300

    init

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+async function main(): Promise<void> {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      password: 'test'
+    }
+  })
+
+  await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      password: 'test'
+    }
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
